27de0ac400b765ea21b987eba349552c5720344c
Samson Meseret <80979055+samsonmeseret@users.noreply.github.com>
2022-11-15 20:18:34 +0000

    little update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 import Main from "./components/Main";
 
+
 import Footer from "./components/Footer";
 import "./commenResource/css/styles.css";
 import "./commenResource/css/bootstrap.css";
