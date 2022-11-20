63bc2aee2366facf28d2a54124b7c1ed314cab67
samsonmeseret <samsonmeseretb@gmail.com>
2022-11-20 15:32:06 -0500

    refactoring the Nav Link

---
+++ src/components/NavData.js
@@ -0,0 +1,37 @@
+import search from "../commenResource/images/icons/search-icon-sm.png";
+import cart from "../commenResource/images/icons/cart-sm.png";
+
+let NavData = [
+  {
+    link: "Mac",
+  },
+  {
+    link: "Iphone",
+  },
+  {
+    link: "Ipad",
+  },
+  {
+    link: "Watch",
+  },
+  {
+    link: "tv",
+  },
+  {
+    link: "Music",
+  },
+  {
+    link: "Support",
+  },
+  {
+    img: search,
+  },
+  {
+    img: cart,
+  },
+  //   {
+  //     img: "../commenResource/images/icons/logo-sm.png",
+  //   },
+];
+
+export default NavData;
---
+++ src/components/NavLink.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class NavLink extends Component {
+  render() {
+    return (
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="/mac/">
+          {this.props.link.link ? (
+            this.props.link.link
+          ) : (
+            <img src={this.props.link.img} />
+          )}
+          {console.log(this.props.link)}
+        </a>
+      </li>
+    );
+  }
+}
